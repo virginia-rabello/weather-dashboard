557ad8f6906965180dc3f595004d68d04599c6f5
Virginia <virginia.rabello2107@gmail.com>
2021-04-23 16:15:38 -0500

    able to display city current climate

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 var key = config.key;
 var url = config.url;
-fetch("https://yahoo-weather5.p.rapidapi.com/weather?location=sunnyvale&format=json&u=f", {
+var cityInfoDiv = document.querySelector("#cityInfoDiv");
+var cityForecast = document.querySelector("#cityForecast");
+var cityTarget = "";
+
+var getCoordinates = function (city){
+fetch("https://yahoo-weather5.p.rapidapi.com/weather?location="+ city +"&format=json&u=f", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": key,
@@ -9,9 +14,64 @@ fetch("https://yahoo-weather5.p.rapidapi.com/weather?location=sunnyvale&format=j
 })
 .then(response => response.json())
 .then(response => {
-	console.log(response.current_observation.atmosphere.humidity);
+    console.log(response);
+	cityTarget = response.location.city;
+    var lat = response.location.lat;
+    var long = response.location.long;
+    searchCity(lat, long);
+})
+.catch(err => {
+	console.error(err);
+});
+}
+
+var searchCity = function (lat, long){
+fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+ lat +"&lon="+ long +"&exclude=hourly,daily&appid=" + openAPI)
+	
+
+.then(response => response.json())
+.then(response => {
+    console.log(response);
+       
+    var cityH2 = document.getElementById("cityInfo");
+    var temp = "Temp: " + response.current.temp +" °F" ;
+    var wind = "Wind: " + response.current.wind_speed + " MPH";
+    var humidity = "Humidity: " + response.current.humidity + "%";
+    var uvText = "UV Index: ";
+    var uvIndex = response.current.uvi;
+    var uvSpan = document.getElementById("uvIndex");
+    var cityTemp= document.getElementById("temp");
+    var cityWind= document.getElementById("wind");
+    var cityHumidity= document.getElementById("humi");
+    var cityUV= document.getElementById("uv");
+    var date = new Date(response.current.dt * 1000);
+    var datePlace = document.getElementById("currentDate");
+    console.log(cityH2);
+    cityH2.innerHTML = cityTarget;
+    datePlace.innerHTML = date;
+    cityTemp.innerHTML = temp;
+    cityWind.innerHTML = wind;
+    cityHumidity.innerHTML = humidity;
+    cityUV.innerHTML = uvText;
+    uvSpan.textContent = uvIndex;
+
+    	
     
 })
 .catch(err => {
 	console.error(err);
 });
+}
+var buttonHandler = function(event){
+    var target = event.target;
+    var city = document.getElementById("form").value;
+    if(target.id === "search"){
+        if(city){
+            getCoordinates(city);
+        }
+    }
+
+}
+
+addEventListener("click", buttonHandler);
+
