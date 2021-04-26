d4fc0fcd4fe3cd0fafc6965ecf80aad23f5450a7
Virginia <virginia.rabello2107@gmail.com>
2021-04-25 22:38:52 -0500

    5 day forecast done

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,14 @@ var key = config.key;
 var url = config.url;
 var cityIcon = document.querySelector("#icon");
 var cityForecast = document.querySelector("#cityForecast");
+var day1 = document.querySelector("#day1");
+var day2 = document.querySelector("#day2");
+var day3 = document.querySelector("#day3");
+var day4 = document.querySelector("#day4");
+var day5 = document.querySelector("#day5");
 var cityTarget = "";
 var cities = [];
+var arrayDays = [day1, day2, day3, day4, day5];
 var lastSearch = "";
 
 var saveCities = function() {
@@ -21,6 +27,12 @@ var loadCities = function() {
     lastSearch = JSON.parse(localStorage.getItem("lastCity"));
 };
 
+var dateFormat = function(date){
+     var formated_date = date.getMonth()+1 + "/" + date.getDate() + 
+    "/" + date.getFullYear();
+    return formated_date;
+};
+
 var sameCityDetector = function (city){
     var push = true;
     if(cities){
@@ -35,6 +47,21 @@ var sameCityDetector = function (city){
     }
     return push;
 } 
+
+var calculateUV = function (UVindex){
+    var indexColor;
+    if(UVindex >= 6){
+        indexColor = "red";
+    }
+    else if(UVindex > 2 && UVindex < 6){
+        indexColor = "yellow";
+    }
+    else {
+        indexColor = "green";
+    }
+    return indexColor;
+       
+}
 
 var generateIconURL = function (code){
   
@@ -71,13 +98,14 @@ fetch("https://yahoo-weather5.p.rapidapi.com/weather?location="+ city +"&format=
 }
 
 var searchCity = function (lat, long){
-fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+ lat +"&lon="+ long +"&exclude=hourly,daily&appid=" + openAPI)
+fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+ lat +"&lon="+ long +"&units=imperial&exclude=minutely,hourly&appid=" + openAPI)
 	
 
 .then(response => response.json())
 .then(response => {
     console.log(response);
-       
+
+    // Today's weather   
     var cityH2 = document.getElementById("cityInfo");
     var temp = "Temp: " + response.current.temp +" °F" ;
     var wind = "Wind: " + response.current.wind_speed + " MPH";
@@ -96,10 +124,6 @@ fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+ lat +"&lon="+ long
         code:response.current.weather[0].icon
     }
     var URLicon = generateIconURL(icon.code);
-    cityIcon.style.width = '120px';
-    cityIcon.style.height = '120px';
-    cityIcon.style.marginLeft = '100px';
-    cityIcon.style.marginTop = '20px';
     cityIcon.src = URLicon;
     cityH2.innerHTML = cityTarget;
     datePlace.innerHTML = date;
@@ -107,10 +131,24 @@ fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+ lat +"&lon="+ long
     cityWind.innerHTML = wind;
     cityHumidity.innerHTML = humidity;
     cityUV.innerHTML = uvText;
-    uvSpan.textContent = uvIndex;
+    var UVcolor = calculateUV(uvIndex);
+    uvSpan.style.backgroundColor = UVcolor;
+    uvSpan.textContent = uvIndex; 
     
-    	
-    
+    // 5 days forecast
+    for(var i = 0; i < arrayDays.length; i++){
+        var dayDate = new Date (response.daily[i].dt * 1000);
+        var dayFormat = dateFormat(dayDate);
+        var dayIcon = generateIconURL(response.daily[i].weather[0].icon);
+        var dayTemp = "Temp: " + response.daily[i].temp.day +" °F" ;
+        var dayWind = "Wind: " + response.daily[i].wind_speed + " MPH";
+        var dayHumidity = "Humidity: " + response.daily[i].humidity + "%";
+        arrayDays[i].children[0].innerHTML = dayFormat;
+        arrayDays[i].children[1].src = dayIcon;
+        arrayDays[i].children[2].innerHTML = dayTemp;
+        arrayDays[i].children[3].innerHTML = dayWind;
+        arrayDays[i].children[4].innerHTML = dayHumidity;
+    }
 })
 .catch(err => {
 	console.error(err);
